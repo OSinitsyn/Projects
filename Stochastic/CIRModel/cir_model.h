bcf539519a3baef8b1aa76868e50ab30f7fdcdca
Oleksandr Sinitsyn <Oleksandr.Sinitsyn@gmail.com>
2018-03-21 00:07:34 -0400

    Initial commit

---
+++ Stochastic/CIRModel/cir_model.h
@@ -0,0 +1,35 @@
+/********************************************************************************************************
+*
+* FILENAME :
+*     cir_model.h
+*
+* DESCRIPTION :
+*
+* PUBLIC MEMBER FUNCTIONS :
+*
+* NOTES :
+*
+* AUTHOR : Oleksandr Sinitsyn
+*
+* START DATE : 20 Mar 2018
+*
+* CHANGES :
+*
+**********************************************************************************************************/
+
+#ifndef CIR_MODEL_H_
+#define CIR_MODEL_H_
+
+#include <vector>
+#include <cmath>
+#include <random>
+
+class CIRModel {
+  private:
+  public:
+    CIRModel();
+    CIRModel(const CIRModel& other);
+    ~CIRModel();
+};
+
+#endif // CIR_MODEL_H_
