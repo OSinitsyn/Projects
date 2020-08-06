eaebfe48e400286ab8f08f903fd5689683e6ddb6
Oleksandr Sinitsyn <Oleksandr.Sinitsyn@gmail.com>
2020-08-06 19:18:40 -0400

    Added plotting with mplfinance library

--- Analytics/Equities/stock_data_loader.py
+++ Analytics/Equities/stock_data_loader.py
@@ -1,26 +1,49 @@
 # A simple yfinance data loader
 # https://pypi.org/project/yfinance/
 
+# Plotting data using the mplfinance libray
+# https://pypi.org/project/mplfinance/
+
 import yfinance as yf
 import matplotlib.pyplot as plt
+import mplfinance as mpf
 
 class StockDataLoader:
-    def __init__(self, tickers):
+    def __init__(self, tickers, period, interval):
         self.tickers = tickers
+        self.period = period
+        self.interval = interval
+        self.tickersDf = None
 
     def loadData(self):
-        return yf.download(tickers = self.tickers, period = '1d', interval = '1m')
+        self.tickersDf = yf.download(tickers = self.tickers, period = self.period, interval = self.interval, group_by = 'ticker')
+        return self.tickersDf
+
+    def tickerData(self, ticker):
+        if ticker in self.tickers:
+            return self.tickersDf[ticker]
+
+class StockDataPlot:
+    def __init__(self):
+        pass
+
+    def plotData(self, tickerDf):
+        mpf.plot(tickerDf, type = 'candle', style = 'yahoo')
+        plt.show()
 
 
 if __name__ == '__main__':
-    tickers = 'AAPL'
-    sdLoader = StockDataLoader(tickers)
+    tickers = ['AAPL', 'MS']
+    sdLoader = StockDataLoader(tickers, '1d', '1m')
 
     tickersDf = sdLoader.loadData()
+    print(tickersDf.shape)
+    #tickersDf
+
     print(tickersDf[-20:])
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    tickersDf['Open'].plot(ax=ax, label='Open', title=tickers, lw=1.0)
-    ax.legend(loc='best')
-    plt.show()
+    tickerDf = sdLoader.tickerData('MS')
+    print(tickerDf[-20:])
+
+    sdPlot = StockDataPlot()
+    sdPlot.plotData(tickerDf)
