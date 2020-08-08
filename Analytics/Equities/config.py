2bd0f0840a8cd35284b9f2f8158cd5f3abc7cb6c
Oleksandr Sinitsyn <Oleksandr.Sinitsyn@gmail.com>
2020-08-07 21:01:08 -0400

    A config file for stock data loader. Initial commit.

---
+++ Analytics/Equities/config.py
@@ -0,0 +1,6 @@
+class StockDataConfig:
+    def getExportDataPath(self):
+        return '/Users/Oleks/Documents/Python/Projects/Analytics/Equities/TempData/'
+
+    def getLogDataPath(self):
+        return '/Users/Oleks/Documents/Python/Projects/Analytics/Equities/LogData/'
