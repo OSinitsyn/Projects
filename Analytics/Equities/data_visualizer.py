48d8e66919d2c157cb1dae8212df3ad82fc698b3
Oleksandr Sinitsyn <Oleksandr.Sinitsyn@gmail.com>
2020-08-10 23:21:34 -0400

    Data visualizer. Initial commit.

---
+++ Analytics/Equities/data_visualizer.py
@@ -0,0 +1,14 @@
+# Plotting data using the mplfinance libray
+# https://pypi.org/project/mplfinance/
+
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import logger
+
+class TimeSeriesPlot:
+    def __init__(self):
+        pass
+
+    def plotTickerData(self, tickerDf):
+        mpf.plot(tickerDf, type = 'candle', style = 'yahoo')
+        plt.show()
