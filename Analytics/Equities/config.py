e28f20a1926dd3ccf64f78d82d3df604927045f4
Oleksandr Sinitsyn <Oleksandr.Sinitsyn@gmail.com>
2020-08-11 21:14:07 -0400

    Return dictionaries with config params using getParams() method.

--- Analytics/Equities/config.py
+++ Analytics/Equities/config.py
@@ -1,6 +1,24 @@
-class StockDataConfig:
-    def getExportDataPath(self):
-        return '/Users/Oleks/Documents/Python/Projects/Analytics/Equities/TempData/'
+class HistDataConfig:
+    ### Historical data loader ###
+    # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+    # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+    @staticmethod
+    def getParams():
+        return {'period' : '1d',
+                'interval' : '1m',
+                'group_by' : 'ticker',
+                'auto_adjust' : True,
+                'prepost' : False,
+                'threads' : True ,
+                'export_data_path' : '/Users/Oleks/Documents/Python/Projects/Analytics/Equities/TempData/',
+                'sep' : ',',
+                'na_rep' : '',
+                'header' : True }
 
-    def getLogDataPath(self):
-        return '/Users/Oleks/Documents/Python/Projects/Analytics/Equities/LogData/'
+
+class LoggerConfig:
+    ### Logger config ####
+    @staticmethod
+    def getParams():
+        return {'write_log_files' : True,
+                'log_data_path' : '/Users/Oleks/Documents/Python/Projects/Analytics/Equities/LogData/'}
