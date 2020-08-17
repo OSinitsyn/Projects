0c6c54e14db5b42b5e2dbd94b0283ad1543ca27e
Oleksandr Sinitsyn <Oleksandr.Sinitsyn@gmail.com>
2020-08-16 23:57:54 -0400

    Added mean() and std() calculation for returns.

--- Analytics/Equities/hist_returns.py
+++ Analytics/Equities/hist_returns.py
@@ -5,10 +5,18 @@ class HistReturns:
         self.logReturns = True
         self.colName = 'Adj Close' #
 
-    def Calculate(self, tickerDf):
+    def calcReturns(self, tickerDf):
         if self.logReturns:
             tickerDf['Return'] = np.log(tickerDf[self.colName]/tickerDf[self.colName].shift(1))
         else:
             tickerDf['Return'] = tickerDf[self.colName].pct_change()
 
         return tickerDf
+
+    def calcReturnsMean(self, tickerDf):
+        if 'Return' in tickerDf:
+            return tickerDf['Return'].mean()
+
+    def calcReturnsSTD(self, tickerDf):
+        if 'Return' in tickerDf:
+            return tickerDf['Return'].std()
